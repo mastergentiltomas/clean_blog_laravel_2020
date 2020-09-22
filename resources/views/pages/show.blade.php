7041b0f2d26f5084df929bbe4f5025ceaedf026e
schwanen <tomas.schwanen@outlook.fr>
2020-09-22 09:14:16 +0200

    route par défaut

---
+++ resources/views/pages/show.blade.php
@@ -0,0 +1,4 @@
+<h1>{{ $pages->titre }}</h1>
+<div>
+  {{ $pages->texte }}
+</div>
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+// ROUTE DE BASE --------------------------
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// ROUTE DES PAGES --------------------------
+use App\Http\Controllers\PagesController;
+Route::get('/', [PagesController::class, 'show'])->name('homepage');
+Route::get('/pages/{id}/{slug}', [PagesController::class, 'show'])
+      ->where([
+        'id' => '[1-9][0-9]*',
+        'slug' => '[a-z0-9][a-z0-9\-]*'
+      ])
+      ->name('pages.show');
+//Route::resource('pages', PagesController::class)->only(['index', 'show']);
